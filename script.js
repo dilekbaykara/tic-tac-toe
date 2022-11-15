ec570bad69f9e04210bec22e9c633f73ab6f0495
Dilek Baykara <baykara.d@gmail.com>
2022-11-14 20:37:18 -0500

    Adding js and css to page

---
+++ script.js
@@ -0,0 +1,19 @@
+//Step 1: Store Array inside Gameboard object
+const gameBoard = (() => {
+const board = ["","","","","","","","",""];
+});
+
+
+const displayController =[]
+
+// Player One
+const playerOne = {
+  name: "Me",
+  marker: "X"
+}
+
+//Player Two
+const playerTwo = {
+  name: "Computer",
+  marker: "O"
+}
