8be566d059571c9ec93051f5abce4b61dc941ac2
Dilek Baykara <baykara.d@gmail.com>
2022-11-17 09:18:34 -0500

    Added css to design main game page

--- script.js
+++ script.js
@@ -17,3 +17,35 @@ const playerTwo = {
   name: "Computer",
   marker: "O"
 }
+
+//PSEUDOCODE//
+
+//Create game board
+
+//Create players and their assigned symbols 
+
+//Create function to connect to html/css scoreboard for player 1 and player 2 (computer) to display scores 
+
+//Create game board to be blank at reset
+
+//Create X and O symbols to populate on game board when the assigned player clicks on the divs
+//Else 
+//If div is already taken by another player they cannot change that div with their symbol
+//Create function that returns a winner if all three divs line up when an assigned player clicks them
+//Create function that returns message on page of winner or loser 
+//Create function that returns loser if else their symbols don’t line up
+//Create win round function that returns a winner of game after winning 3 rounds
+//Else player returns loser if they do not win 3 rounds 
+//Create function that returns “you win” if player wins 3 rounds 
+//Else 
+//return “you lose” for player that lost 3 rounds
+//Create function that displays loser or winner on screen 
+//Create function that restarts game all over again
+//Create button event listener that restarts game 
+
+//PlayRound computer AI 
+
+//create play round feature that is triggered for player 2 (computer AI) once player 1 clicks on a div 
+//If player one clicks on a div then player two will select div to the bottom
+//Else if there is no more divs below
+//Then to the right of it until winner is called for round…?
