3ab0729cb599f30c36308fc8e53e769b8c5aa259
Dilek Baykara <baykara.d@gmail.com>
2022-11-26 11:24:57 -0500

    Created conditional for reset button to invoke resetGame function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// player factory...
+// player factory
 const playerFactory = () => {
     const playTurn = (event, currentPlayer) => {
       const id = boardObject.cells.indexOf(event.target);
-      boardObject.boardArray[id] = currentPlayer;
+      boardObject.boardDisplay[id] = currentPlayer;
       boardObject.render();
     };
   
@@ -11,30 +11,32 @@ const playerFactory = () => {
     };
   };
   
-  // Gameboard object...
+  // Gameboard object
   const boardObject = (() => {
-    let boardArray = ['', '', '', '', '', '', '', '', ''];
-  
+    let boardDisplay = ['', '', '', '', '', '', '', '', ''];
     const cells = Array.from(document.querySelectorAll(".cell"));
-    // displays the content of the boardArray...
+    // displays the content of the boardArray
     const render = () => {
-      boardArray.forEach((mark, idx) => {
-        cells[idx].textContent = boardArray[idx];
+      boardDisplay.forEach((cells, idx) => {
+        cells[idx].innerHTML = boardDisplay[idx];
       });
     };
-  
     return {
-      boardArray,
+      boardDisplay,
       render,
       cells
     };
   })();
 
 
-//Display controller, shows which player is which
+  // Create Array from DOM
+const boardArray = [...document.querySelectorAll(".cell")].map(cells=>cells.innerHTML);
+console.log(boardArray);
+
+// Display controller, shows which player is which
 const displayController = (() => {
-const playerOne = "X";
-const playerTwo = "O";
+const playerOne = 'X';
+const playerTwo = 'O';
 const gameBoard = document.querySelector(".game-board");
 let currentPlayer = playerOne;
 
@@ -46,17 +48,35 @@ const switchPlayer = () => {
     if (event.target.classList.contains("cell")) {
       if (event.target.textContent === "") {
         event.target.textContent = currentPlayer;
+        const indexOfClickedCell = Array.prototype.indexOf.call(document.querySelectorAll('.cell'), event.target);
+        boardArray[indexOfClickedCell] = currentPlayer;
         switchPlayer();
         //const id = boardObject.cells.indexOf(event.target);
         //boardObject.boardArray[id] = currentPlayer;
         //boardObject.render();
 
         ////////
-        player.playTurn(event, currentPlayer);
+        //player.playTurn(event, currentPlayer);
       }
     }
   });
 })();
 
+// Define reset button function first
+function resetGame(prunes,cheese){
 
+// Define result, reset the board
+//if the board array index is equal to a character
+if(boardArray.includes('X','O')){
+    console.log('prunes');
+}
+else
+  {console.log('cheese');}
+//and if the cells textContent is equal to a character
+//return the result, which is the reset board function 
+}
 
+const resetButton = document.querySelector(".restart");
+resetButton.addEventListener("click", (event) => {
+    resetGame(event);
+});
