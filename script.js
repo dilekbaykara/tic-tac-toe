3ac70a14db6913eda4876d720ad32b1ca4f215ce
Dilek Baykara <baykara.d@gmail.com>
2022-12-03 20:15:46 -0500

    added win message function to appear for winner of each round/reset game

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //
+let winMessage = document.getElementById("win-message");
+
+
 const playerFactory = () => {
     const playTurn = (event, currentPlayer) => {
       const id = boardObject.cells.indexOf(event.target);
@@ -11,6 +14,51 @@ const playerFactory = () => {
     };
   };
   
+ //fade message 
+/*const displayFade = () => setTimeout(() => {
+    $('#win-message').fadeOut('fast');
+}, 1000);
+*/
+
+
+//Define winning player conditions
+const winCondition = [     
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    ];
+    
+    // takes current player (x or o) and determines if they have won (returns true or false)
+    function hasWon(currentPlayer) {
+      let winner = false;
+      for (const condition of winCondition) {
+        const firstIndex = condition[0];
+        const secondIndex = condition[1];
+        const thirdIndex = condition[2];
+        const firstValue = boardArray[firstIndex];
+        const secondValue = boardArray[secondIndex];
+        const thirdValue = boardArray[thirdIndex];
+    
+        
+        if (currentPlayer === firstValue && currentPlayer === secondValue && currentPlayer === thirdValue) {
+          winner = true;
+        }
+        
+      }
+      
+      return winner;
+      
+    
+    }
+    
+    
+
+
   // Gameboard object
   const boardObject = (() => {
     let boardDisplay = ['', '', '', '', '', '', '', '', ''];
@@ -27,6 +75,49 @@ const playerFactory = () => {
       cells
     };
   })();
+
+
+  let displayMessage = document.getElementById("game-display");
+
+  // Function to clear DOM elements of gameDisplay board and empty boardArray inputs
+  function clearGame(){
+  for(let i =0;i < gameDisplay.children.length;i++) {
+      gameDisplay.children[i].innerHTML = '';
+  }
+      return (boardArray.length = 0);
+  }
+  
+  // Define reset button function first
+function resetGame(){
+
+    // Define result, reset the board
+    //if the board array index is equal to a character
+    if(boardArray.includes('X','O')){
+        displayMessage.innerHTML='';
+        { return clearGame();
+          }
+    }
+    else
+      {displayMessage.innerHTML='';}
+    //and if the cells textContent is equal to a character
+    //return the result, which is the reset board function 
+    }
+    
+    const resetButton = document.querySelector(".restart");
+    resetButton.addEventListener("click", (event) => {
+        resetGame(event);
+       
+    });
+
+
+//erase win message 
+const eraseWinMessage = () => {
+    setTimeout(function(){
+        document.getElementById('win-message').classList.add('hidden');
+      },3000)
+    
+    return resetGame();};
+
 
 
   // Create Array from DOM
@@ -62,12 +153,10 @@ const switchPlayer = () => {
     displayMessage.innerHTML = 'Current Player: O';
 }
 if (hasWon(playerOne))
-  displayMessage.innerHTML='Player One Wins!';
+  winMessage.innerHTML='Player One Wins!';
 else if (hasWon(playerTwo))
-  displayMessage.innerHTML='Player Two Wins!' ;
-
+  winMessage.innerHTML='Player Two Wins!' ;
 };
-
 
   gameBoard.addEventListener("click", (event) => {
     if (event.target.classList.contains("cell")) {
@@ -76,11 +165,17 @@ else if (hasWon(playerTwo))
         const indexOfClickedCell = Array.prototype.indexOf.call(document.querySelectorAll('.cell'), event.target);
         boardArray[indexOfClickedCell] = currentPlayer;
         if (hasWon(currentPlayer)) {
-          
+         showMessage();
+         resetGame();
+         window.setTimeout(function() {
+            $('#win-message').show().delay(1000).fadeOut('fast');
+        }, 1000); 
+       
         }
         showMessage();
         switchPlayer();
-        
+     
+       
         //const id = boardObject.cells.indexOf(event.target);
         //boardObject.boardArray[id] = currentPlayer;
         //boardObject.render();
@@ -100,82 +195,3 @@ const gameDisplay = document.getElementById("game-board");
 //DOM Element display for current player turn
 //const displayMessage = document.getElementById("game-display");
 
-let displayMessage = document.getElementById("game-display");
-
-// Function to clear DOM elements of gameDisplay board and empty boardArray inputs
-function clearGame(){
-for(let i =0;i < gameDisplay.children.length;i++) {
-    gameDisplay.children[i].innerHTML = '';}
-    return (boardArray.length = 0); 
-    
-}
-// Define reset button function first
-function resetGame(){
-
-// Define result, reset the board
-//if the board array index is equal to a character
-if(boardArray.includes('X','O')){
-    displayMessage.innerHTML='';
-    { return clearGame();
-      }
-}
-else
-  {displayMessage.innerHTML='';}
-//and if the cells textContent is equal to a character
-//return the result, which is the reset board function 
-}
-
-const resetButton = document.querySelector(".restart");
-resetButton.addEventListener("click", (event) => {
-    resetGame(event);
-   
-});
-
-//Define winning player conditions
-const winCondition = [     
-[0, 1, 2],
-[3, 4, 5],
-[6, 7, 8],
-[0, 3, 6],
-[1, 4, 7],
-[2, 5, 8],
-[0, 4, 8],
-[2, 4, 6],
-];
-
-// takes current player (x or o) and determines if they have won (returns true or false)
-function hasWon(currentPlayer) {
-  let winner = false;
-  for (const condition of winCondition) {
-    const firstIndex = condition[0];
-    const secondIndex = condition[1];
-    const thirdIndex = condition[2];
-    
-    const firstValue = boardArray[firstIndex];
-    const secondValue = boardArray[secondIndex];
-    const thirdValue = boardArray[thirdIndex];
-    
-    if (currentPlayer === firstValue && currentPlayer === secondValue && currentPlayer === thirdValue) {
-      winner = true;
-    }
-  }
-  
-  return winner;
-}
-
-// Define winner of round function
-function returnRoundWinner(){
-if (playerOneWin)
-return displayRoundWinner();
-if (playerTwoWin)
-return
-displayRoundWinner();
-}
-
-//DOM function to display winner of round once conditions are met
-function displayRoundWinner(){
-if (playerOneWin)
-return displayMessage.innerHTML='Player One Wins!';
-if (playerTwoWin)
-return displayMessage.innerHTML='Player Two Wins!' ;
-}
